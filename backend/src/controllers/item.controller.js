2622d8a3b1296513d30620aab1b1db9ef4466cce
Vishal Naidu <vminorr@gmail.com>
2026-03-14 17:38:02 +0530

    Add getItemsController and update routes for fetching items

--- backend/src/controllers/item.controller.js
+++ backend/src/controllers/item.controller.js
@@ -21,3 +21,18 @@ export async function itemController(req, res){
         item
     })
 }
+
+export async function getItemsController(req, res){
+    const items = await itemModel.find()
+
+    if(!items){
+        return res.status(404).json({
+            message: "Items not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "Items fetched successfully",
+        items
+    })
+}
--- backend/src/routes/item.routes.js
+++ backend/src/routes/item.routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { itemController } from '../controllers/item.controller.js'
+import { getItemsController, itemController } from '../controllers/item.controller.js'
 
 const itemRouter = express.Router()
 
+// POST /api/item/save
 itemRouter.post("/save", itemController)
 
+// GET /api/item/get-items
+itemRouter.get("/get-item", getItemsController)
 export default itemRouter;
